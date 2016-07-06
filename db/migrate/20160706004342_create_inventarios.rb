57fd68782536a999c3867171a391314b04913e11
KathyFC <katherine.castaneda@softtek.com>
2016-07-05 19:54:22 -0500

    Lógica a nivel de base de datos2

--- db/migrate/20160706004342_create_inventarios.rb
+++ db/migrate/20160706004342_create_inventarios.rb
@@ -1,9 +1,11 @@
 class CreateInventarios < ActiveRecord::Migration
   def change
     create_table :inventarios do |t|
-      t.string :NumSerie
+      t.string :NumSerie, unique: true, null: false
       t.integer :wheelSize
       t.string :description
+
+
 
       t.timestamps null: false
     end
